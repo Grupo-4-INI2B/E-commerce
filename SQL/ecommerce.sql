f285c44387e85ebdcf3f29bbd283bd2f6bcad438
Heitor Lima dos Santos Cunha <heitor.lima@unesp.br>
2023-09-08 19:48:39 -0300

    Correção de nomenclatura.

--- SQL/ecommerce.sql
+++ SQL/ecommerce.sql
@@ -54,36 +54,36 @@
         SELECT * FROM tbl_produto;
 
     /*Consultas com filtros*/
-        SELECT * FROM tbl_cliente WHERE id_usuario = 1;
+        SELECT * FROM tbl_cliente WHERE id_cliente = 1;
         SELECT * FROM tbl_pedido WHERE id_pedido = 1;
         SELECT * FROM tbl_produto WHERE id_produto = '10wa';
     
     /*Consultas com ordenação*/
-        SELECT * FROM tbl_cliente ORDER BY nome_usuario;
+        SELECT * FROM tbl_cliente ORDER BY nome_cliente;
         SELECT * FROM tbl_pedido ORDER BY vlr_total;
         SELECT * FROM tbl_produto ORDER BY nome;
 
     /*Consultas com agrupamento*/
-        SELECT * FROM tbl_cliente GROUP BY nome_usuario;
+        SELECT * FROM tbl_cliente GROUP BY nome_cliente;
         SELECT * FROM tbl_pedido GROUP BY vlr_total;
         SELECT * FROM tbl_produto GROUP BY nome;
     
     /*Consultas com junção*/
-        SELECT * FROM tbl_cliente INNER JOIN tbl_pedido ON tbl_cliente.id_usuario = tbl_pedido.cliente;
+        SELECT * FROM tbl_cliente INNER JOIN tbl_pedido ON tbl_cliente.id_cliente = tbl_pedido.cliente;
         SELECT * FROM tbl_pedido INNER JOIN tbl_produto ON tbl_pedido.produto = tbl_produto.id_produto;
-        SELECT * FROM tbl_cliente INNER JOIN tbl_produto ON tbl_cliente.id_usuario = tbl_produto.id_produto;
+        SELECT * FROM tbl_cliente INNER JOIN tbl_produto ON tbl_cliente.id_cliente = tbl_produto.id_produto;
     
     /*Consultas com junção e agrupamento*/  
-        SELECT * FROM tbl_cliente INNER JOIN tbl_pedido ON tbl_cliente.id_usuario = tbl_pedido.cliente GROUP BY nome_usuario;
+        SELECT * FROM tbl_cliente INNER JOIN tbl_pedido ON tbl_cliente.id_cliente = tbl_pedido.cliente GROUP BY nome_cliente;
         SELECT * FROM tbl_pedido INNER JOIN tbl_produto ON tbl_pedido.produto = tbl_produto.id_produto GROUP BY vlr_total;
-        SELECT * FROM tbl_cliente INNER JOIN tbl_produto ON tbl_cliente.id_usuario = tbl_produto.id_produto GROUP BY nome;
+        SELECT * FROM tbl_cliente INNER JOIN tbl_produto ON tbl_cliente.id_cliente = tbl_produto.id_produto GROUP BY nome;
 
     /*Consultas com junção e ordenação*/
-        SELECT * FROM tbl_cliente INNER JOIN tbl_pedido ON tbl_cliente.id_usuario = tbl_pedido.cliente ORDER BY nome_usuario;
+        SELECT * FROM tbl_cliente INNER JOIN tbl_pedido ON tbl_cliente.id_cliente = tbl_pedido.cliente ORDER BY nome_cliente;
         SELECT * FROM tbl_pedido INNER JOIN tbl_produto ON tbl_pedido.produto = tbl_produto.id_produto ORDER BY vlr_total;
-        SELECT * FROM tbl_cliente INNER JOIN tbl_produto ON tbl_cliente.id_usuario = tbl_produto.id_produto ORDER BY nome;
+        SELECT * FROM tbl_cliente INNER JOIN tbl_produto ON tbl_cliente.id_cliente = tbl_produto.id_produto ORDER BY nome;
 
     /*Consultas com junção, agrupamento e ordenação*/
-        SELECT * FROM tbl_cliente INNER JOIN tbl_pedido ON tbl_cliente.id_usuario = tbl_pedido.cliente GROUP BY nome_usuario ORDER BY nome_usuario;
+        SELECT * FROM tbl_cliente INNER JOIN tbl_pedido ON tbl_cliente.id_cliente = tbl_pedido.cliente GROUP BY nome_cliente ORDER BY nome_cliente;
         SELECT * FROM tbl_pedido INNER JOIN tbl_produto ON tbl_pedido.produto = tbl_produto.id_produto GROUP BY vlr_total ORDER BY vlr_total;
-        SELECT * FROM tbl_cliente INNER JOIN tbl_produto ON tbl_cliente.id_usuario = tbl_produto.id_produto GROUP BY nome ORDER BY nome;          
+        SELECT * FROM tbl_cliente INNER JOIN tbl_produto ON tbl_cliente.id_cliente = tbl_produto.id_produto GROUP BY nome ORDER BY nome;          
