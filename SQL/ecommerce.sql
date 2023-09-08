0fc25c41286df5581954e8cdfec20166c7a78906
Heitor Lima dos Santos Cunha <142709058+ToiLima@users.noreply.github.com>
2023-09-07 23:34:42 -0300

    Ajuste com que foi pedido para o Banco de Dados.

--- SQL/ecommerce.sql
+++ SQL/ecommerce.sql
@@ -1,26 +1,31 @@
 CREATE TABLE tbl_cliente(
     id_usuario serial PRIMARY KEY,
-    usuario varchar(30) NOT NULL,
+    nome_usuario varchar(30) NOT NULL,
     email varchar(30) NOT NULL,
     senha varchar(30) NOT NULL,
-    telefone varchar(20),
-    pedido int not null,
+    telefone varchar(20) NOT NULL,
+    pedidos serial NOT NULL,
 );
 
 CREATE TABLE tbl_pedido(
     id_pedido serial PRIMARY KEY,
     vlr_total float NOT NULL, /*Valor total do pedido*/
-    produto int not null,
-    cliente int not null,
+    produto int NOT NULL,
+    cliente int NOT NULL,
 );
 
 CREATE TABLE tbl_produtos(
     id_produto serial PRIMARY KEY,
-    nome varchar(10) NOT NULL,
+    nome text(10) NOT NULL,
+    descricao text(100) NOT NULL, 
     vlr float NOT NULL,
-    categoria varchar(10) NOT NULL, /*filme, anime e etc*/
     qnt int,
-    tipo varchar(10) NOT NULL /*BUTTON OU STICKER*/
+    exluido varchar(10),
+    dta_exclusao timestamp,
+    id_visual varchar(50),
+    custo numeric(10, 2),
+    margem_lucro numeric(10, 2),
+    imagem varchar(50)
 );
 
 ALTER TABLE tbl_cliente ADD CONSTRAINT pedido FOREIGN KEY (pedido) REFERENCES tbl_pedido(id_pedido);
