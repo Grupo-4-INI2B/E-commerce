56fd811b785ae1870cabcf38eaf178f5ea3f3bcc
Igor Zamparo Gomes <igor.gomes@unesp.br>
2023-10-22 20:00:02 -0300

    Perfil .php gay

--- Site/HTML/Perfil.php
+++ Site/HTML/Perfil.php
@@ -1,17 +1,41 @@
-<?php
-    ini_set ('display_errors', 1);
-    error_reporting (E_ALL);
-    session_start();
-    include ("../PHP/Funcoes.php");
 
-    if(isset($_SESSION['sessaoUsuario'])) {
-        $sessaoUsuario = $_SESSION['sessaoUsuario'];
-        $nome = $_SESSION['nome'];
-        $adm = $_SESSION['adm'];
-    }else {
-        header("Location: Login.php");
-        exit();
-    }
+
+<!DOCTYPE html>
+<html lang="pt-BR">    
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Byte Craft - Perfil</title>
+    <link rel="stylesheet" href="../CSS/login.css">
+</head>
+
+<body>
+    <form name="frmLogin" method="post" action="../PHP/Login_usuario.php">
+    <div class="main-login">
+        <div class="login-container">
+
+            <div class="left-login">
+                <img src="../Imagens/logocaixinhabranco.svg">
+
+                <h1>Bem-vindo usuário!<br></h1>
+                <h3>Este é seu perfil.</h3>
+            </div>
+            <div class="right-login">
+                <div class="card-login">
+                    <?php
+                    ini_set ('display_errors', 1);
+                    error_reporting (E_ALL);
+                    session_start();
+                     include ("../PHP/Funcoes.php");
+
+                        if(isset($_SESSION['sessaoUsuario'])) {
+                             $sessaoUsuario = $_SESSION['sessaoUsuario'];
+                             $nome = $_SESSION['nome'];
+                             $adm = $_SESSION['adm'];
+                         }else {
+                          header("Location: Login.php");
+                          exit();
+                                }
 
     echo"
         <!DOCTYPE html>
@@ -32,7 +56,8 @@
                 <h2 class='campo-perfil'>Nome : $nome</h2>
                 <h2 class='campo-perfil'>Email : $sessaoUsuario</h2>
                 <a class='btn-logout' href='../PHP/Logout_usuario.php'>Logout</a>
-                <a class='btn-logout' href='Excluir_usuario.php'>Exterminar Perfil</a>
+                <br>
+                <a class='btn-logout' href='Excluir_usuario.php'>Excluir Perfil</a>
             </body>
         </html>";
     }else {
@@ -46,4 +71,10 @@
     }
 
 ?>
-  
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+</body>
+</html>
